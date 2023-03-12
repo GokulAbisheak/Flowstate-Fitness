421caaae9f1785d348047da244699842d74576ee
QuaisNM <quaisro@gmail.com>
2023-03-12 18:20:54 +0530

    Check

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ import connectDatabase from "./config/database.js";
 
 const PORT = process.env.PORT || 8090;
 
+const trainerRouter = require("../server/routes/Trainer"); 
+app.use ("/trainer",trainerRouter);
+http://localhost:8090/trainer
+
 app.listen(PORT, () => {
     logger.info(`Server has started and running on PORT ${PORT}`);
     connectDatabase();
