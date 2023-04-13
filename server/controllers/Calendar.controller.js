c9d3464313d5fd90286ace0ed8bb952e863d3b8a
BinuriGunawardena <binuri.gunawardena@gmail.comgit config --global user.name BinuriGunawardena>
2023-04-13 14:35:07 +0530

    Created Calendar model

---
+++ server/controllers/Calendar.controller.js
@@ -0,0 +1,12 @@
+import Calendar from '../models/Calendar.model';
+
+const CalendarController ={
+
+    //Add a session
+    addSession: (req, res) => {
+        const title = req.body.title;
+        const date = Date(req.body.date);
+
+
+    }
+}
