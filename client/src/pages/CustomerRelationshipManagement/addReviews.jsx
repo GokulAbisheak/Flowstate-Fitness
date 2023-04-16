57022610cfd27230acf7940c9846b47ab8133831
lakitha04 <lakithadilshan08@gmail.com>
2023-04-16 11:11:47 +0530

    updated addReviews.jsx

--- client/src/pages/CustomerRelationshipManagement/addReviews.jsx
+++ client/src/pages/CustomerRelationshipManagement/addReviews.jsx
@@ -1,89 +1,110 @@
-import  React,{useState} from 'react';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import FormControl from '@mui/joy/FormControl';
-import FormLabel from '@mui/joy/FormLabel';
-import Textarea from '@mui/joy/Textarea';
-import IconButton from '@mui/joy/IconButton';
-import Menu from '@mui/joy/Menu';
-import MenuItem from '@mui/joy/MenuItem';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import FormatBold from '@mui/icons-material/FormatBold';
-import FormatItalic from '@mui/icons-material/FormatItalic';
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
-import Check from '@mui/icons-material/Check';
+import React, { useState } from 'react';
+import { Card, Button, Container, Form, Row, Col } from 'react-bootstrap'
 
-export default function addReviews() {
-  const [italic, setItalic] = React.useState(false);
-  const [fontWeight, setFontWeight] = React.useState('normal');
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+import axios from 'axios'
+
+const addReview = () => {
+  // review rating  description
+  const [reviews, setReviews] = useState([])
+  const [rating, setRating] = useState(0)
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+
+    const getSingleReviewData = async () => {
+      const { data } = await axios.get('/api/reviews/add')
+      console.log(data)
+
+      setDescription(data.description)
+      setReviews(data.review)
+
+
+    }
+    getSingleReviewData()
+
+  })
+  const addReviewHandler = async (e) => {
+
+    e.preventDefault()
+
+    let review = {
+      product_id: id,
+      rating: rating,
+      description: description
+    }
+
+    await axios.post(`/api/Review/addReview/${id}`, review)
+
+    history.push('/Review')
+  }
+
   return (
-    <FormControl>
-      <FormLabel>Your comment</FormLabel>
-      <Textarea
-        placeholder="Type something here…"
-        minRows={3}
-        endDecorator={
-          <Box
-            sx={{
-              display: 'flex',
-              gap: 'var(--Textarea-paddingBlock)',
-              pt: 'var(--Textarea-paddingBlock)',
-              borderTop: '1px solid',
-              borderColor: 'divider',
-              flex: 'auto',
-            }}
-          >
-            <IconButton
-              variant="plain"
-              color="neutral"
-              onClick={(event) => setAnchorEl(event.currentTarget)}
-            >
-              <FormatBold />
-              <KeyboardArrowDown fontSize="md" />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={() => setAnchorEl(null)}
-              size="sm"
-              placement="bottom-start"
-              sx={{ '--ListItemDecorator-size': '24px' }}
-            >
-              {['200', 'normal', 'bold'].map((weight) => (
-                <MenuItem
-                  key={weight}
-                  selected={fontWeight === weight}
-                  onClick={() => {
-                    setFontWeight(weight);
-                    setAnchorEl(null);
-                  }}
-                  sx={{ fontWeight: weight }}
-                >
-                  <ListItemDecorator>
-                    {fontWeight === weight && <Check fontSize="sm" />}
-                  </ListItemDecorator>
-                  {weight === '200' ? 'lighter' : weight}
-                </MenuItem>
-              ))}
-            </Menu>
-            <IconButton
-              variant={italic ? 'soft' : 'plain'}
-              color={italic ? 'primary' : 'neutral'}
-              aria-pressed={italic}
-              onClick={() => setItalic((bool) => !bool)}
-            >
-              <FormatItalic />
-            </IconButton>
-            <Button sx={{ ml: 'auto' }}>Send</Button>
-          </Box>
-        }
-        sx={{
-          minWidth: 300,
-          fontWeight,
-          fontStyle: italic ? 'italic' : 'initial',
-        }}
-      />
-    </FormControl>
-  );
+    <>
+      <Container className="mt-10 p-4">
+        <h2 className='text-center'>Add Review</h2>
+        <hr />
+
+
+        <Row>
+
+          <Col md={4} lg={4} sm={4}>
+
+            <Form onSubmit={addReviewHandler}>
+              <Form.Group className="mb-3" controlId="rating">
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
+                  type="number"
+                />
+              </Form.Group>
+
+
+
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  as="textarea"
+                />
+              </Form.Group>
+
+
+              <Button variant="primary" type="submit">
+                Add Review
+              </Button>
+            </Form>
+
+            <br />
+
+            <h5>Product Reviews</h5>
+            <hr />
+
+            {reviews.length > 0 ? (
+              reviews.map(review => {
+                return <p key={review.id}>Rating: {review.rating} <br /> {review.description}</p>
+              })
+            ) : (<p> No reviews for this product </p>)}
+
+
+          </Col>
+        </Row>
+
+
+
+
+      </Container>
+
+
+
+
+
+    </>
+  )
+
 }
+
+
+
+export default addReview;
