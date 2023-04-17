711e3fd61843c384f0397206c7636c23915c0dce
QuaisNM <quaisro@gmail.com>
2023-04-17 13:54:42 +0530

    Comments added

--- server/app.js
+++ server/app.js
@@ -11,13 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 
+// Add console.log statement here
+console.log('Middleware applied');
+
 app.get('/', (req, res) => {
+    // Add console.log statement here
+    console.log('Request received at root endpoint');
     res.send('Server is Running! 🚀');
 })
 
 app.use('/user', userRouter);
 app.use('/admin', adminRouter);
 app.use('/review', reviewRouter);
-app.use('/Trainer',trainerRouter);
+app.use('/trainer', trainerRouter);
+
+// Add console.log statement here
+console.log('Routers assigned');
 
 export default app;
