2d6b2360c89ebab7c85cbc7e74a34cc2a9a9bf30
IT21375132 <gokulabisheak12@gmail.com>
2023-04-20 03:38:02 +0530

    Imported ScanMembership and Profile Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,8 @@ import Cart from './pages/ProductManagement/Cart';
 import TrainerCalendar from './pages/PersonalTrainingManagement/TrainerCalendar';
 import Attendance from './pages/PersonalTrainingManagement/Attendance';
 import AddAttendance from './pages/PersonalTrainingManagement/AddAttendance';
-
+import ScanMembership from './pages/UserManagement/ScanMembership';
+import Profile from './pages/UserManagement/Profile';
 
 
 function App() {
@@ -53,6 +54,7 @@ function App() {
               <Route path="/admin/schedule" element={<TrainerCalendar/>}/>
               <Route path="/admin/attendance" element={<Attendance/>}/>
               <Route path="/admin/addAttendance" element={<AddAttendance/>}/>
+              <Route path="/admin/scan" element={<ScanMembership />}/>
               <Route path="/button" element={<Main />} />
             </Route>
 
@@ -64,6 +66,7 @@ function App() {
               <Route path="/user/updateReviews" element={<UpdateReviews/>}/>
               <Route path="/user/deleteReviews" element={<DeleteReviews/>}/>
               <Route path="/user/cart" element={<Cart/>} />
+              <Route path="/user/profile" element={<Profile />} />
 
             </Route>
             <Route path="*" element={<NotFound />} />
