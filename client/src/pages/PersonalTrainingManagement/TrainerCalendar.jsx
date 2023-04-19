29695b52d1138545c535967e2946237b9e16c26c
BinuriGunawardena <binuri.gunawardena@gmail.comgit config --global user.name BinuriGunawardena>
2023-04-19 05:32:24 +0530

    Updated

--- client/src/pages/PersonalTrainingManagement/TrainerCalendar.jsx
+++ client/src/pages/PersonalTrainingManagement/TrainerCalendar.jsx
@@ -7,11 +7,11 @@ import axios from 'axios';
 
 const Calendar = () => {
 
-  const [sessions, setSessions] = useState([]);
-  const [title, setTitle] = useState("");
-  const [start, setStartDate] = useState("");
-  const [end, setEndDate] = useState("");
-  const [description, setDescription] = useState("");
+  const [events, setEvents] = useState([]);
+  // const [title, setTitle] = useState("");
+  // const [start, setStartDate] = useState("");
+  // const [end, setEndDate] = useState("");
+  // const [description, setDescription] = useState("");
 
   const handleDateSelect = (selectInfo) => {
     const title = prompt('Enter session title:');
@@ -23,17 +23,9 @@ const Calendar = () => {
         end: selectInfo.endStr,
         description,
       };
-      axios.post('http://localhost:8090/session/add', sessionData).then(() => {
-        alert('Successfull!');
-            setTitle(res.data.title);
-            setStartDate(res.data.start);
-            setEndDate(res.data.end);
-            setDescription(res.data.description);
-      }
-
-      )
+      axios.post('http://localhost:8090/session/add', sessionData)
         .then(res => {
-          setSessions([...sessions, res.data]);
+          setEvents([...events, res.data]);
         })
         .catch(err => {
           console.error(err);
@@ -46,12 +38,12 @@ const Calendar = () => {
     const title = prompt('Edit event title:', eventInfo.event.title);
     const description = prompt('Edit event description:', eventInfo.event.extendedProps.description);
     if (title) {
-      const sessionData = {
-        ...sessionInfo.session.extendedProps,
+      const eventData = {
+        ...eventInfo.event.extendedProps,
         title,
         description,
       };
-      axios.put(`http://localhost:8090/session/update/${sessionInfo.session.id}`, eventData)
+      axios.put(`http://localhost:8090/session/update/${eventInfo.event.id}`, eventData)
         .then(res => {
           setEvents(events.map(event => (event.id === res.data.id ? res.data : event)));
         })
