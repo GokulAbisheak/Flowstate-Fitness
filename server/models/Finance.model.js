b0d7d337dda6dc1242cd6791dee967ce3ebc5969
Sankeethan Y <99704338+IT21228162@users.noreply.github.com>
2023-04-19 19:18:14 +0530

    Update and rename Finance.models.js to Finance.model.js

---
+++ server/models/Finance.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const financeSchema = new mongoose.Schema({
+
+    salaryID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    department: {
+        type: String,
+        required: true
+    },
+
+
+    lastUpdated: {
+        type: Date,
+        required: true
+    },
+
+    salary: {
+        type: Number,
+        required: true
+    },
+
+    frequency: {
+        type: String,
+        required: true
+    },
+
+   
+});
+
+const Finance = mongoose.model('Finance', financeSchema);
+
+export default Finance;
+
