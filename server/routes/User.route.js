95087f0fbb1b0d7c431432e9c83b0133d93420ea
IT21375132 <gokulabisheak12@gmail.com>
2023-04-24 01:15:14 +0530

    Added Search Function to User

--- server/routes/User.route.js
+++ server/routes/User.route.js
@@ -4,9 +4,10 @@ import protect from '../middlewares/authMiddleware.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/', protect, UserController.getAllUsers);
+userRouter.get('/', UserController.getAllUsers);
 userRouter.get('/:email', UserController.getUserByEmail);
 userRouter.get('/auth/me', UserController.getMe);
+userRouter.get('/search/byemail', UserController.searchUser);
 userRouter.post('/add', UserController.createUser);
 userRouter.patch('/update/:email', UserController.updateUserByEmail);
 userRouter.delete('/delete/:email', UserController.deleteUserByEmail);
