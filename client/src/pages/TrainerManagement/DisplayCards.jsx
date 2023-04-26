46be358ec12c535bdb75ffeb9014acf8e2eef1ad
“QuaisNM” <quaisro@gmail.com>
2023-04-26 13:01:04 +0530

    Display  cards

--- client/src/pages/TrainerManagement/DisplayCards.jsx
+++ client/src/pages/TrainerManagement/DisplayCards.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Grid } from '@mui/material';
+import { Card,CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 
-const TrainerScreen = () => {
-    /*const [allTrainers, setAllTrainers] = useState([]);
+
+
+
+const DisplayCards = () => {
+    const [allTrainers, setAllTrainers] = useState([]);
+    const [email, setEmail] = useState("");
 
     useEffect(() => {
         const getAllTrainers = () => {
@@ -15,22 +19,33 @@ const TrainerScreen = () => {
             })
         }
         getAllTrainers();
-    }, [])*/
+    }, [])
 
     
     return(
-        /*<Grid container spacing={2}>
+        <Grid container spacing={2}>
             {allTrainers.map((trainer) => (
-                <Grid item key={trainer.id} xs={12} sm={6} md={4}>
-                    <Card />
+                <Grid item key={trainer.email} xs={12} sm={6} md={4}>
+                 <Card sx={{height:"100%"}}>
+                    <CardActionArea sx={{height: '100%' }}>
+                        
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
+                                Name: {trainer.firstName} {trainer.lastName}
+                                </Typography>
+                                <Typography variant="" gutterBottom sx={{ textAlign: 'center' }}>
+                                Name: {trainer.firstName} {trainer.LastName}
+                                </Typography>
+                            </CardContent>
+                        
+                    </CardActionArea>
+
+                 </Card>
                 </Grid>
             ))}
-        </Grid>*/
-        <>
-            <h1>All Students</h1>
-        </>
+        </Grid>
     );
 
 }
 
-export default TrainerScreen;
+export default DisplayCards;
